e0c4e74486f4fdc4bc08204318a525961a1b6cc2
fatimasif <fatimasif2004@gmail.com>
2023-02-07 23:11:02 -0500

    completed task C of lab 2

--- edit-array.cpp
+++ edit-array.cpp
@@ -19,36 +19,39 @@ and go back to the step 3. Otherwise, if index i is out of range, the program ex
 #include <iostream>
 using namespace std;
 
-const int capacity = 10;
+const int capacity = 10;    // the CAPACITY is the the amount of elements the array can hold and it is set with "const"
 
 int main() {
-    int myData[capacity];
+        // creating the array and variables
+    int myData[capacity];    // the array myData holds 10 elements (CAPACITY)
 
-    // Fill array with value 1
+        // creating a for loop to fill all of the array's elements with value 1
     for (int i = 0; i < capacity; i++) {
         myData[i] = 1;
     }
 
+        // creating variables for the index and value the user will input
     int index;
     int value;
+        // creating a do while loop
     do {
-            // Print array
+            // creating a for loop to print the value of each element in the array 
         for (int i = 0; i < capacity; i++) {
             cout << myData[i] << endl;
         }
 
-            // Get index and value from user
+            // get the index and value from user
         cout << "Input index: " << endl; 
         cin >> index;
-        cout << "Input value:60 " << endl;
+        cout << "Input value: " << endl;
         cin >> value;
 
-            // Update array at index i if i is valid
+            // use an if statement to update the array at index if the index is valid
         if (index >= 0 && index < capacity) {
             cout << "The new array is: " << endl;
             myData[index] = value;
         }
-    } while (index >= 0 && index < capacity);
+    } while (index >= 0 && index < capacity);   // continue until the index no longer works
 
     return 0;
 }
