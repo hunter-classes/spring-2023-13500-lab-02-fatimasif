e5577fd6bcd3f89ea31b8b62e0bab7ecd2308c02
fatimasif <fatimasif2004@gmail.com>
2023-02-07 23:37:54 -0500

    completed task D of lab 2

---
+++ fibonacci.cpp
@@ -0,0 +1,36 @@
+/*
+Author: Fatima Asif
+Course: CSCI-135
+Instructor: M. Zamansky
+Assignment: fibonacci.cpp, task D, lab-02
+
+Write a program fibonacci.cpp, which uses an array of ints to compute and
+print all Fibonacci numbers from F(0) to F(59).
+*/
+
+#include <iostream>
+using namespace std;
+
+    // creating a constant value for the array 
+const int capacity = 60;
+
+int main()
+{
+        // make an array
+    int fib[capacity];
+        // first two terms are given
+    fib[0] = 0;
+    fib[1] = 1;
+        // use a for loop to go through each element in the array
+    for(int i = 2; i < capacity; i++){
+       fib[i] = fib[i-1] + fib[i-2];    // and all the following ones can be computed iteratively 
+    }
+        // print the fibonacci numbers 
+    cout << "The fibonacci numbers are:" << endl;
+    for(int i = 0; i < capacity; i++){  // use a for loop to go through the array 
+        cout << fib[i] << endl;
+    }
+
+    return 0;
+    
+}
